6e3e8292b214cc655efb26d11a637eb0f7b77026
labhanshu03 <143610953+labhanshu03@users.noreply.github.com>
2025-08-26 22:17:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const port=process.env.PORT ||5000
 app.use(cookieParser())
 app.use(express.json()) 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://echo-room-frontend1.onrender.com",
     credentials:true,
       
 }))
